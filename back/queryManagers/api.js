e29d9fb8e4743f43c13ea93da9cec2b6945d4698
Ozeliurs <ozeliurs@gmail.com>
2024-01-22 17:52:49 +0100

    fix: change prettier conf

--- back/queryManagers/api.js
+++ back/queryManagers/api.js
@@ -1,7 +1,7 @@
 // Main method, exported at the end of the file. It's the one that will be called when a REST request is received.
 function manageRequest(request, response) {
-  response.statusCode = 200;
-  response.end(`Thanks for calling ${request.url}`);
+    response.statusCode = 200
+    response.end(`Thanks for calling ${request.url}`)
 }
 
 /* This method is a helper in case you stumble upon CORS problems. It shouldn't be used as-is:
@@ -10,20 +10,20 @@ function manageRequest(request, response) {
  ** Access-Control-Allow-Headers is an example of how to authorize some headers, the ones given in this example
  ** are probably not the ones you will need. */
 function addCors(response) {
-  // Website you wish to allow to connect to your server.
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  // Request methods you wish to allow.
-  response.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-  );
-  // Request headers you wish to allow.
-  response.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type",
-  );
-  // Set to true if you need the website to include cookies in the requests sent to the API.
-  response.setHeader("Access-Control-Allow-Credentials", true);
+    // Website you wish to allow to connect to your server.
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    // Request methods you wish to allow.
+    response.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    )
+    // Request headers you wish to allow.
+    response.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-Requested-With,content-type'
+    )
+    // Set to true if you need the website to include cookies in the requests sent to the API.
+    response.setHeader('Access-Control-Allow-Credentials', true)
 }
 
-exports.manage = manageRequest;
+exports.manage = manageRequest
