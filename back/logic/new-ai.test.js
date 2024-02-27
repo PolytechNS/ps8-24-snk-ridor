4ea577180d0e7975afc75dddc42e88da2b960cca
Ozeliurs <ozeliurs@gmail.com>
2024-02-27 11:26:45 +0100

    fix: undo prettier
    Ref #16

--- back/logic/new-ai.test.js
+++ back/logic/new-ai.test.js
@@ -3,13 +3,7 @@ const { describe, test } = require('mocha');
 const chai = require('chai');
 
 function getBoard() {
-    return JSON.parse(
-        JSON.stringify(
-            Array.from({ length: 10 }, () =>
-                Array.from({ length: 10 }, () => 0)
-            )
-        )
-    );
+    return JSON.parse(JSON.stringify(Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => 0))));
 }
 
 describe('AStar', () => {
@@ -44,18 +38,8 @@ describe('AStar', () => {
             const goalNodes = aStar.goals;
             chai.expect(goalNodes).to.be.an('array');
             chai.expect(goalNodes.length).to.equal(9);
-            // deep equan in any order
-            chai.expect(goalNodes).to.contain.deep.members([
-                [1, 9],
-                [2, 9],
-                [3, 9],
-                [4, 9],
-                [5, 9],
-                [6, 9],
-                [7, 9],
-                [8, 9],
-                [9, 9],
-            ]);
+            // prettier-ignore
+            chai.expect(goalNodes).to.contain.deep.members([[1, 9], [2, 9], [3, 9], [4, 9], [5, 9], [6, 9], [7, 9], [8, 9], [9, 9]]);
         });
 
         test('when player is 2', () => {
@@ -63,18 +47,8 @@ describe('AStar', () => {
             const goalNodes = aStar.goals;
             chai.expect(goalNodes).to.be.an('array');
             chai.expect(goalNodes.length).to.equal(9);
-            // deep equan in any order
-            chai.expect(goalNodes).to.contain.deep.members([
-                [1, 1],
-                [2, 1],
-                [3, 1],
-                [4, 1],
-                [5, 1],
-                [6, 1],
-                [7, 1],
-                [8, 1],
-                [9, 1],
-            ]);
+            // prettier-ignore
+            chai.expect(goalNodes).to.contain.deep.members([[1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1]]);
         });
     });
 
@@ -83,157 +57,27 @@ describe('AStar', () => {
             board[1][1] = 1;
         });
 
+        // prettier-ignore
         let testSet = [
-            {
-                x: 5,
-                y: 5,
-                walls: [],
-                expected: [
-                    [5, 4],
-                    [5, 6],
-                    [4, 5],
-                    [6, 5],
-                ],
-                label: 'No walls',
-            },
+            { x: 5, y: 5, walls: [], expected: [[5, 4], [5, 6], [4, 5], [6, 5]], label: 'No walls' },
 
             // Edges
-            {
-                x: 1,
-                y: 5,
-                walls: [],
-                expected: [
-                    [1, 4],
-                    [1, 6],
-                    [2, 5],
-                ],
-                label: 'Left Edge',
-            },
-            {
-                x: 9,
-                y: 5,
-                walls: [],
-                expected: [
-                    [9, 4],
-                    [9, 6],
-                    [8, 5],
-                ],
-                label: 'Right Edge',
-            },
-            {
-                x: 5,
-                y: 1,
-                walls: [],
-                expected: [
-                    [5, 2],
-                    [4, 1],
-                    [6, 1],
-                ],
-                label: 'Top Edge',
-            },
-            {
-                x: 5,
-                y: 9,
-                walls: [],
-                expected: [
-                    [5, 8],
-                    [4, 9],
-                    [6, 9],
-                ],
-                label: 'Bottom Edge',
-            },
+            { x: 1, y: 5, walls: [], expected: [[1, 4], [1, 6], [2, 5]], label: 'Left Edge' },
+            { x: 9, y: 5, walls: [], expected: [[9, 4], [9, 6], [8, 5]], label: 'Right Edge' },
+            { x: 5, y: 1, walls: [], expected: [[5, 2], [4, 1], [6, 1]], label: 'Top Edge' },
+            { x: 5, y: 9, walls: [], expected: [[5, 8], [4, 9], [6, 9]], label: 'Bottom Edge' },
 
             // Normal Walls
-            {
-                x: 5,
-                y: 5,
-                walls: [[''.concat(5).concat(5), 0]],
-                expected: [
-                    [4, 5],
-                    [6, 5],
-                    [5, 6],
-                ],
-                label: 'Bottom Right Wall',
-            },
-            {
-                x: 5,
-                y: 5,
-                walls: [[''.concat(5).concat(6), 0]],
-                expected: [
-                    [4, 5],
-                    [6, 5],
-                    [5, 4],
-                ],
-                label: 'Top Right Wall',
-            },
-            {
-                x: 5,
-                y: 5,
-                walls: [[''.concat(5).concat(5), 1]],
-                expected: [
-                    [4, 5],
-                    [5, 6],
-                    [5, 4],
-                ],
-                label: 'Right Bottom Wall',
-            },
-            {
-                x: 5,
-                y: 5,
-                walls: [[''.concat(4).concat(5), 1]],
-                expected: [
-                    [5, 4],
-                    [6, 5],
-                    [5, 6],
-                ],
-                label: 'Left Bottom Wall',
-            },
+            { x: 5, y: 5, walls: [[''.concat(5).concat(5), 0]], expected: [[4, 5], [6, 5], [5, 6]], label: 'Bottom Right Wall' },
+            { x: 5, y: 5, walls: [[''.concat(5).concat(6), 0]], expected: [[4, 5], [6, 5], [5, 4]], label: 'Top Right Wall' },
+            { x: 5, y: 5, walls: [[''.concat(5).concat(5), 1]], expected: [[4, 5], [5, 6], [5, 4]], label: 'Right Bottom Wall' },
+            { x: 5, y: 5, walls: [[''.concat(4).concat(5), 1]], expected: [[5, 4], [6, 5], [5, 6]], label: 'Left Bottom Wall' },
 
             // "Overhanging" Walls
-            {
-                x: 5,
-                y: 5,
-                walls: [[''.concat(4).concat(5), 0]],
-                expected: [
-                    [4, 5],
-                    [6, 5],
-                    [5, 6],
-                ],
-                label: 'Bottom Left Wall',
-            },
-            {
-                x: 5,
-                y: 5,
-                walls: [[''.concat(4).concat(6), 0]],
-                expected: [
-                    [4, 5],
-                    [6, 5],
-                    [5, 4],
-                ],
-                label: 'Top Left Wall',
-            },
-            {
-                x: 5,
-                y: 5,
-                walls: [[''.concat(5).concat(6), 1]],
-                expected: [
-                    [4, 5],
-                    [5, 6],
-                    [5, 4],
-                ],
-                label: 'Right Top Wall',
-            },
-            {
-                x: 5,
-                y: 5,
-                walls: [[''.concat(4).concat(6), 1]],
-                expected: [
-                    [5, 4],
-                    [6, 5],
-                    [5, 6],
-                ],
-                label: 'Left Top Wall',
-            },
+            { x: 5, y: 5, walls: [[''.concat(4).concat(5), 0]], expected: [[4, 5], [6, 5], [5, 6]], label: 'Bottom Left Wall' },
+            { x: 5, y: 5, walls: [[''.concat(4).concat(6), 0]], expected: [[4, 5], [6, 5], [5, 4]], label: 'Top Left Wall' },
+            { x: 5, y: 5, walls: [[''.concat(5).concat(6), 1]], expected: [[4, 5], [5, 6], [5, 4]], label: 'Right Top Wall' },
+            { x: 5, y: 5, walls: [[''.concat(4).concat(6), 1]], expected: [[5, 4], [6, 5], [5, 6]], label: 'Left Top Wall' },
 
             // Walls on the edges TODO
         ];
--- back/logic/new-ai.ts
+++ back/logic/new-ai.ts
@@ -67,10 +67,7 @@ function updateBoard(gameState: GameState): Promise<boolean> {
 
 // === END OF AI ===
 
-function getPlayerCoordinates(
-    board: number[][],
-    player: number
-): [number, number] {
+function getPlayerCoordinates(board: number[][], player: number): [number, number] {
     for (let i = 1; i < 10; i++) {
         for (let j = 1; j < 10; j++) {
             if (board[i][j] === player) {
@@ -80,12 +77,7 @@ function getPlayerCoordinates(
     }
 }
 
-function getWallAtCoordinates(
-    walls: [string, number][],
-    x: number,
-    y: number,
-    orientation: number
-): [string, number] {
+function getWallAtCoordinates(walls: [string, number][], x: number, y: number, orientation: number): [string, number] {
     for (let i = 0; i < walls.length; i++) {
         if (walls[i][0] === `${x}${y}` && walls[i][1] === orientation) {
             return walls[i];
@@ -108,29 +100,9 @@ class AStar {
         this.start = getPlayerCoordinates(board, player);
 
         if (player === 2) {
-            this.goals = [
-                [1, 1],
-                [2, 1],
-                [3, 1],
-                [4, 1],
-                [5, 1],
-                [6, 1],
-                [7, 1],
-                [8, 1],
-                [9, 1],
-            ];
+            this.goals = [[1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1]]; // prettier-ignore
         } else {
-            this.goals = [
-                [1, 9],
-                [2, 9],
-                [3, 9],
-                [4, 9],
-                [5, 9],
-                [6, 9],
-                [7, 9],
-                [8, 9],
-                [9, 9],
-            ];
+            this.goals = [[1, 9], [2, 9], [3, 9], [4, 9], [5, 9], [6, 9], [7, 9], [8, 9], [9, 9]]; // prettier-ignore
         }
     }
 
@@ -147,22 +119,11 @@ class AStar {
         if (x > 1) {
             // Check if there is a top wall if y > 1
             // Check if there is a bottom wall if y < 9
+            // prettier-ignore
             if (
                 !(
-                    (y > 1 &&
-                        getWallAtCoordinates(
-                            this.walls,
-                            x - 1,
-                            y,
-                            Direction.VERTICAL
-                        )) ||
-                    (y < 9 &&
-                        getWallAtCoordinates(
-                            this.walls,
-                            x - 1,
-                            y + 1,
-                            Direction.VERTICAL
-                        ))
+                    y > 1 && getWallAtCoordinates(this.walls, x - 1, y, Direction.VERTICAL) ||
+                    y < 9 && getWallAtCoordinates(this.walls, x - 1, y + 1, Direction.VERTICAL)
                 )
             ) {
                 ret.push([x - 1, y]);
@@ -173,22 +134,11 @@ class AStar {
         if (x < 9) {
             // Check if there is a top wall if y > 1
             // Check if there is a bottom wall if y < 9
+            // prettier-ignore
             if (
                 !(
-                    (y > 1 &&
-                        getWallAtCoordinates(
-                            this.walls,
-                            x,
-                            y,
-                            Direction.VERTICAL
-                        )) ||
-                    (y < 9 &&
-                        getWallAtCoordinates(
-                            this.walls,
-                            x,
-                            y + 1,
-                            Direction.VERTICAL
-                        ))
+                    y > 1 && getWallAtCoordinates(this.walls, x, y, Direction.VERTICAL) ||
+                    y < 9 && getWallAtCoordinates(this.walls, x, y + 1, Direction.VERTICAL)
                 )
             ) {
                 ret.push([x + 1, y]);
@@ -199,22 +149,11 @@ class AStar {
         if (y < 9) {
             // Check if there is a left wall if x > 1
             // Check if there is a right wall if x < 9
+            // prettier-ignore
             if (
                 !(
-                    (x > 1 &&
-                        getWallAtCoordinates(
-                            this.walls,
-                            x - 1,
-                            y + 1,
-                            Direction.HORIZONTAL
-                        )) ||
-                    (x < 9 &&
-                        getWallAtCoordinates(
-                            this.walls,
-                            x,
-                            y + 1,
-                            Direction.HORIZONTAL
-                        ))
+                    x > 1 && getWallAtCoordinates(this.walls, x - 1, y + 1, Direction.HORIZONTAL) ||
+                    x < 9 && getWallAtCoordinates(this.walls, x, y + 1, Direction.HORIZONTAL)
                 )
             ) {
                 ret.push([x, y + 1]);
@@ -225,22 +164,11 @@ class AStar {
         if (y > 1) {
             // Check if there is a left wall if x > 1
             // Check if there is a right wall if x < 9
+            // prettier-ignore
             if (
                 !(
-                    (x > 1 &&
-                        getWallAtCoordinates(
-                            this.walls,
-                            x - 1,
-                            y,
-                            Direction.HORIZONTAL
-                        )) ||
-                    (x < 9 &&
-                        getWallAtCoordinates(
-                            this.walls,
-                            x,
-                            y,
-                            Direction.HORIZONTAL
-                        ))
+                    x > 1 && getWallAtCoordinates(this.walls, x - 1, y, Direction.HORIZONTAL) ||
+                    x < 9 && getWallAtCoordinates(this.walls, x, y, Direction.HORIZONTAL)
                 )
             ) {
                 ret.push([x, y - 1]);
@@ -257,10 +185,7 @@ class AStar {
         let fScore: { [key: string]: number } = {};
 
         gScore[`${this.start[0]}${this.start[1]}`] = 0;
-        fScore[`${this.start[0]}${this.start[1]}`] = this.heuristic(
-            this.start,
-            this.goals[0]
-        );
+        fScore[`${this.start[0]}${this.start[1]}`] = this.heuristic(this.start, this.goals[0]);
 
         console.log('Initial gScore and fScore:', gScore, fScore);
 
@@ -278,51 +203,27 @@ class AStar {
 
             console.log('Current node:', current);
 
-            if (
-                this.goals.some(
-                    (goal) => goal[0] === current[0] && goal[1] === current[1]
-                )
-            ) {
+            if (this.goals.some((goal) => goal[0] === current[0] && goal[1] === current[1])) {
                 console.log('Goal reached:', current);
                 return this.reconstructPath(cameFrom, current);
             }
 
-            openSet = openSet.filter(
-                (node) => node[0] !== current[0] || node[1] !== current[1]
-            );
+            openSet = openSet.filter((node) => node[0] !== current[0] || node[1] !== current[1]);
 
             for (let neighbor of this.neighbors(current)) {
                 let neighborStr = `${neighbor[0]}${neighbor[1]}`;
                 let tentativeGScore = gScore[currentStr] + 1;
 
-                console.log(
-                    'Evaluating neighbor:',
-                    neighbor,
-                    'with tentative G Score:',
-                    tentativeGScore
-                );
+                console.log('Evaluating neighbor:', neighbor, 'with tentative G Score:', tentativeGScore);
 
                 if (tentativeGScore < gScore[neighborStr]) {
                     cameFrom[neighborStr] = current;
                     gScore[neighborStr] = tentativeGScore;
-                    fScore[neighborStr] =
-                        gScore[neighborStr] +
-                        this.heuristic(neighbor, this.goals[0]);
+                    fScore[neighborStr] = gScore[neighborStr] + this.heuristic(neighbor, this.goals[0]);
 
-                    console.log(
-                        'Updated gScore and fScore for neighbor:',
-                        neighbor,
-                        gScore,
-                        fScore
-                    );
+                    console.log('Updated gScore and fScore for neighbor:', neighbor, gScore, fScore);
 
-                    if (
-                        !openSet.some(
-                            (node) =>
-                                node[0] === neighbor[0] &&
-                                node[1] === neighbor[1]
-                        )
-                    ) {
+                    if (!openSet.some((node) => node[0] === neighbor[0] && node[1] === neighbor[1])) {
                         openSet.push(neighbor);
                         console.log('Neighbor added to open set:', neighbor);
                     }
@@ -334,10 +235,7 @@ class AStar {
         return [];
     }
 
-    private reconstructPath(
-        cameFrom: { [key: string]: [number, number] },
-        current: [number, number]
-    ): [number, number][] {
+    private reconstructPath(cameFrom: { [key: string]: [number, number] }, current: [number, number]): [number, number][] {
         let totalPath: [number, number][] = [current];
         let currentStr = `${current[0]}${current[1]}`;
 
@@ -351,11 +249,4 @@ class AStar {
     }
 }
 
-export {
-    setup,
-    nextMove,
-    correction,
-    updateBoard,
-    AStar,
-    getWallAtCoordinates,
-};
+export { setup, nextMove, correction, updateBoard, AStar, getWallAtCoordinates };
