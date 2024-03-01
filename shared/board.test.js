b5e50dc16a27b5a82f8bfb5cc59eab0a2913d10f
TristanL06 <tristan.larguier@gmail.com>
2024-03-01 08:28:21 +0100

    to pull

--- shared/board.test.js
+++ shared/board.test.js
@@ -1,1 +1,12 @@
 let { Board } = require('./board.js');
+
+describe('Board', () => {
+    let board;
+    beforeEach(() => {
+        board = new Board();
+    });
+    
+    it('should have 9 cells', () => {
+        expect(board.cells.length).toBe(9);
+    });
+});
