5da78588083b7f2644f022e8698557884e73083d
Ozeliurs <ozeliurs@gmail.com>
2024-04-23 15:08:46 +0200

    feat: make player2 lose elo

--- back/logic/elo.js
+++ back/logic/elo.js
@@ -19,12 +19,16 @@ function updateElo(player1, player2, loser) {
         logger.debug(`Player 1 gains ${gains} elo`);
         user1.elo += gains;
         User.update(user1).then((_) => {});
+        user2.elo -= parseInt(gains / 2);
+        User.update(user2).then((_) => {});
     } else if (winner === 2) {
         let diff = elo1 - elo2;
         let gains = Math.max(20, diff / 10);
         logger.debug(`Player 2 gains ${gains} elo`);
         user2.elo += gains;
         User.update(user2).then((_) => {});
+        user1.elo -= parseInt(gains / 2);
+        User.update(user1).then((_) => {});
     } else {
         logger.warn('Invalid winner');
     }
