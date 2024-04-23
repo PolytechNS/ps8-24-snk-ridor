f91ede7c49c6e2246aa84fe6fb4fd508a1ee75b9
ChatelainClement <clementchatelain@hotmail.fr>
2024-04-23 16:40:02 +0200

    Calcul elo #39

--- back/logic/elo.js
+++ back/logic/elo.js
@@ -26,22 +26,54 @@ function updateEloForUsers(user1, user2, loser) {
 
     logger.debug(`Calculating new elo for player1: ${elo1} and player2: ${elo2} with winner: ${winner}`);
 
+    const eloDifference = Math.abs(elo1 - elo2);
+    let kFactor = 20; // Base K-factor
+
+    // Si l'écart d'ELO est grand, l'échange de points peut être plus important
+    if (eloDifference > 500) {
+        kFactor = 40;
+    } else if (eloDifference > 200) {
+        kFactor = 30;
+    }
+
     if (winner === 1) {
-        let diff = elo2 - elo1;
-        let gains = Math.max(20, diff / 10);
-        logger.debug(`Player 1 gains ${gains} elo`);
-        user1.elo += gains;
+        const expectedScoreWinner = 1 / (1 + Math.pow(10, (elo2 - elo1) / 400));
+        const expectedScoreLoser = 1 / (1 + Math.pow(10, (elo1 - elo2) / 400));
+
+        // Calculez le nouveau ELO
+        let newWinnerElo = Math.max(elo1 + 5, Math.round(elo1 + kFactor * (1 - expectedScoreWinner)));
+        let newLoserElo = Math.min(elo2 - 5, Math.round(elo2 - kFactor * expectedScoreLoser));
+
+        // Assurez-vous que l'ELO ne descende pas en dessous de 0
+        logger.debug(`Player 1 gains ${newWinnerElo} elo`);
+        user1.elo = Math.max(newWinnerElo, 0);
+
+        logger.debug(`Player 2 loses ${newLoserElo} elo`);
+        user2.elo = Math.max(newLoserElo, 0);
+
+        logger.debug(`Player 1 new elo : ${user1.elo} elo`);
+        logger.debug(`Player 2 new elo : ${user2.elo} elo`);
         User.update(user1.email, user1).then((_) => {});
-        user2.elo -= parseInt(gains / 2);
         User.update(user2.email, user2).then((_) => {});
     } else if (winner === 2) {
-        let diff = elo1 - elo2;
-        let gains = Math.max(20, diff / 10);
-        logger.debug(`Player 2 gains ${gains} elo`);
-        user2.elo += gains;
-        User.update(user2.email, user2).then((_) => {});
-        user1.elo -= parseInt(gains / 2);
+        const expectedScoreWinner = 1 / (1 + Math.pow(10, (elo1 - elo2) / 400));
+        const expectedScoreLoser = 1 / (1 + Math.pow(10, (elo2 - elo1) / 400));
+
+        // Calculez le nouveau ELO
+        let newWinnerElo = Math.max(elo2 + 5, Math.round(elo2 + kFactor * (1 - expectedScoreWinner)));
+        let newLoserElo = Math.min(elo1 - 5, Math.round(elo1 - kFactor * expectedScoreLoser));
+
+        // Assurez-vous que l'ELO ne descende pas en dessous de 0
+        logger.debug(`Player 2 wins : ${newWinnerElo} elo`);
+        user2.elo = Math.max(newWinnerElo, 0);
+
+        logger.debug(`Player 1 loses : ${newLoserElo} elo`);
+        user1.elo = Math.max(newLoserElo, 0);
+
+        logger.debug(`Player 2 new elo : ${user2.elo} elo`);
+        logger.debug(`Player 1 new elo : ${user1.elo} elo`);
         User.update(user1.email, user1).then((_) => {});
+        User.update(user2.email, user2).then((_) => {});
     } else {
         logger.debug('Tie, keeping elo');
     }
