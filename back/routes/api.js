249aae1c3b9785b6e0670ce98fd09a6b860a5177
Ozeliurs <ozeliurs@gmail.com>
2024-04-24 13:54:24 +0200

    feat: add achieveùents routes

--- back/routes/api.js
+++ back/routes/api.js
@@ -2,6 +2,7 @@ const auth = require('./auth');
 const friend = require('./friend');
 const user = require('./user');
 const leaderboard = require('./leaderboard');
+const achievements = require('./achievements');
 
 function manageRequest(request, response) {
     let url = new URL(request.url, `http://${request.headers.host}`);
@@ -23,6 +24,10 @@ function manageRequest(request, response) {
 
         case 'leaderboard':
             leaderboard.manageRequest(request, response);
+            break;
+
+        case 'achievements':
+            achievements.manageRequest(request, response);
             break;
 
         default:
