4e855d033a2588325fe0e9d2b9fbc5c741af1a7b
Ozeliurs <ozeliurs@gmail.com>
2024-04-24 14:09:00 +0200

    feat: analysis on achievements

--- back/db/achievements.js
+++ back/db/achievements.js
@@ -3,20 +3,20 @@ const { logger } = require('../libs/logging');
 
 // Achievement ENUM
 const ACHIEVEMENT = {
-    FIRST_GAME: 0, // Have played 1 game
-    PRO_GAMER: 1, // Have played 10 games
-    MASTER_GAMER: 2, // Have played 100 games
-    WINNER: 3, // Have won 1 game
-    PRO_WINNER: 4, // Have won 10 games
-    NOOB: 5, // Have lost 1 game
-    PRO_NOOB: 6, // Have lost 10 games
-    FRIENDLY: 7, // Have played with a friend
-    SOCIAL: 8, // Have played with 10 friends
+    FIRST_GAME: 0, // Have played 1 game // Needs Games Counter
+    PRO_GAMER: 1, // Have played 10 games // Needs Games Counter
+    MASTER_GAMER: 2, // Have played 100 games // Needs Won Games Counter
+    WINNER: 3, // Have won 1 game // Needs Games Counter
+    PRO_WINNER: 4, // Have won 10 games // Needs Games Counter
+    NOOB: 5, // Have lost 1 game // Needs Games Counter
+    PRO_NOOB: 6, // Have lost 10 games // Needs Games Counter
+    FRIENDLY: 7, // Have played with a friend // Needs Games Counter
+    SOCIAL: 8, // Have played with 10 friends // Needs Games Counter
     VELLA: 9, // Have xXx_D4rKV3ll4_xXx in friend
     ONLINE: 10, // Have played an online game
     IA: 11, // Have played an IA game
-    IA_WIN: 12, // Have won an IA game
-    LOCAL: 13, // Have played a local game
+    IA_WIN: 12, // Have won an IA game // IA Games needs Auth now
+    LOCAL: 13, // Have played a local game // #Front Only
     MESSAGE: 14, // Have sent a message to a friend
     EMOTE: 15, // Have used an emote
     BRONZE: 16, // Have 1300 elo
@@ -25,9 +25,9 @@ const ACHIEVEMENT = {
     PLATINUM: 19, // Have 2100 elo
     DIAMOND: 20, // Have 2500 elo
     NO_ELO: 21, // Have 0 elo
-    RICK_ROLL: 22, // Have been rick rolled
-    RICK_ROLLER: 23, // Have rick rolled someone
-    RULES: 24, // Have read the rules
+    RICK_ROLL: 22, // Have been rick rolled // Needs Modifications on messages
+    RICK_ROLLER: 23, // Have rick rolled someone // Needs Modifications on messages
+    RULES: 24, // Have read the rules // #Front Only
 };
 
 class Achievement {
